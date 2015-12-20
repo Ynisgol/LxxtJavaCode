3372f413c67c2411084366aeeb32d0d6c7a38e4c
Ynisgol <ynisgol@gmail.com>
2015-12-19 22:27:39 -0600

    Delete default comment.

--- src/LeetCode/Solution.java
+++ src/LeetCode/Solution.java
@@ -1,8 +1,5 @@
 package LeetCode;
 
-/**
- * Created by Logsiny on 12/19/15.
- */
 public class Solution {
     public static void main(String[] args) {
         System.out.println(new NimGame().canWinNim(5));
