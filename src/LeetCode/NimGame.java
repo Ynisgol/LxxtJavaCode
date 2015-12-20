ae973ca890cb182b7e8bdba4ae17bd870176deab
Ynisgol <ynisgol@gmail.com>
2015-12-19 22:23:50 -0600

    Initial commit

---
+++ src/LeetCode/NimGame.java
@@ -0,0 +1,10 @@
+package LeetCode;
+
+/**
+ * Created by Logsiny on 12/19/15.
+ */
+public class NimGame {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
---
+++ src/LeetCode/Solution.java
@@ -0,0 +1,10 @@
+package LeetCode;
+
+/**
+ * Created by Logsiny on 12/19/15.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new NimGame().canWinNim(5));
+    }
+}
